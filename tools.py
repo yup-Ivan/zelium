c41df216882bef8770e4617b7eeab4a098cbe9ed
yup-Ivan <158217864+yup-Ivan@users.noreply.github.com>
2026-02-02 23:31:29 +0100

    Create tools.py

---
+++ tools.py
@@ -0,0 +1,10 @@
+# Zelium/tools.py
+from selenium.common.exceptions import WebDriverException
+
+def open(url, driver):
+    try:
+        driver.get(url)
+    except WebDriverException as e:
+        print(f"[WebDriverError] No se pudo abrir {url}: {e}")
+    except Exception as e:
+        print(f"[Error inesperado] No se pudo abrir {url}: {type(e).__name__}: {e}")
