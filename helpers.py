c78de9dd8ffb4f8c5d097ec41f8a9a6c55ce5f96
yup-Ivan <158217864+yup-Ivan@users.noreply.github.com>
2026-02-03 13:08:36 +0100

    Update helpers.py

--- helpers.py
+++ helpers.py
@@ -1,4 +1,4 @@
-# Zelium/helpers.py
+# zelium/helpers.py
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
